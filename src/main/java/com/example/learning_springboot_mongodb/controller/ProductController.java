dd53d816683f785d13a3a058ee6ef7aada712e3e
Selim CHIKH ZAOUALI <selim.chikhzaouali@gmail.com>
2024-09-21 17:30:18 +0100

    adding Product controller

---
+++ src/main/java/com/example/learning_springboot_mongodb/controller/ProductController.java
@@ -0,0 +1,42 @@
+package com.example.learning_springboot_mongodb.controller;
+
+import com.example.learning_springboot_mongodb.model.Product;
+import com.example.learning_springboot_mongodb.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Product> getProductById(@PathVariable String id) {
+        return productService.getProductById(id);
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.createProduct(product);
+    }
+
+    @PutMapping("/{id}")
+    public Optional<Product> updateProduct(@PathVariable String id, @RequestBody Product product) {
+        return productService.updateProduct(id, product);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable String id) {
+        productService.deleteProduct(id);
+    }
+}
