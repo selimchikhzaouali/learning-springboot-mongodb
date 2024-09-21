ba8da01796a7dac320de6c80cb78abb3fcb3c4a3
Selim CHIKH ZAOUALI <selim.chikhzaouali@gmail.com>
2024-09-21 16:38:25 +0100

    Initial commit

---
+++ src/main/java/com/example/learning_springboot_mongodb/LearningSpringbootMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.example.learning_springboot_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringbootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringbootMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/learning_springboot_mongodb/LearningSpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learning_springboot_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringbootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
